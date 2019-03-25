7434a584671efb0a880422720cf089445d797947
Joe Shannon <jpshanno@mtu.edu>
2019-03-25 14:07:53 -0400

    Unexported rlang functions

--- R/__Imports.R
+++ R/__Imports.R
@@ -7,23 +7,7 @@
 #' @export
 NULL
 
-# From https://petermeissner.de/blog/2018/04/27/re-exporting-the-pipe/
-#' re-export magrittr pipe operator
-#'
-#' @importFrom magrittr %T>%
-#' @name %T>%
-#' @rdname reexports
-#' @export
-NULL
-
-#' @importFrom rlang !!
+#' @importFrom rlang !! !!! .data
 #' @name !!
 #' @rdname reexports
-#' @export
-NULL
-
-#' @importFrom rlang !!!
-#' @name !!
-#' @rdname reexports
-#' @export
 NULL
